012d08bc840615aed569b085fa9ab4963c2ba766
aleqsunder <al.eq@outlook.com>
2023-04-17 21:10:16 +0400

    BOT-4 Add VkPlay and similar refactoring

---
+++ extends/js/platforms/_list.js
@@ -0,0 +1,13 @@
+import YoutubeAnnouncer from "./youtube-announcer.js"
+import WasdAnnouncer from "./wasd-announcer.js"
+import TrovoAnnouncer from "./trovo-announcer.js"
+import TwitchAnnouncer from "./twitch-announcer.js"
+import VkPlayAnnouncer from "./vk-play-announcer.js"
+
+export default [
+    new YoutubeAnnouncer(),
+    new WasdAnnouncer(),
+    new TrovoAnnouncer(),
+    new TwitchAnnouncer(),
+    new VkPlayAnnouncer(),
+]
