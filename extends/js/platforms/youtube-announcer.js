7e19c18560bf3a9b15270cda4529bef6b7ec0252
aleqsunder <al.eq@outlook.com>
2023-04-17 05:50:55 +0400

    BOT-3 Platform refactor for easy addition of new platforms (#4)

--- extends/js/platforms/youtube-announcer.js
+++ extends/js/platforms/youtube-announcer.js
@@ -1,27 +1,27 @@
 import fetch from "node-fetch"
-import {
-    default_headers as headers,
-    youtube_logo as iconURL,
-    webhook_user,
-    youtube_timeout_limit
-} from "../constants.js"
-import {MessageEmbed} from "discord.js"
+import {default_headers as headers, youtube_timeout_limit} from "../constants.js"
 import {convertApiKeys} from "../helpers.js"
 import BaseAnnouncer from "../base-announcer.js"
 
 const youtubeChannelID = process.env.YOUTUBE_STREAMER_ID
 const youtubeApiKeys = convertApiKeys(process.env.YOUTUBE_API_KEY)
-const discordChannelId = process.env.DISCORD_CHANNEL_ID
 
 export default class YoutubeAnnouncer extends BaseAnnouncer {
+    platformName = 'Youtube'
+    platformLogo = 'https://i.imgur.com/S4DsV5h.png'
+    platformLink = 'https://youtu.be/'
+    platformColor = '#ff0000'
+    channelName = youtubeChannelID
+    interval = youtube_timeout_limit
+    
     async checkStream() {
-        console.log('[youtube] Trying to get a list of user streams ' + youtubeChannelID)
+        this.log(`Trying to get a list of user streams ${this.channelName}`)
         const currentTime = new Date()
-        const interval = youtube_timeout_limit / youtubeApiKeys.length
+        const interval = this.interval / youtubeApiKeys.length
         const currentSecondsInThisHour = (currentTime.getMinutes() * 60 + currentTime.getSeconds())
         const currentYoutubeApiKeyIndex = (currentSecondsInThisHour / (interval / 1000) ^ 0) % youtubeApiKeys.length
         const currentYoutubeApiKey = youtubeApiKeys[currentYoutubeApiKeyIndex]
-        const youtubeLink = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${youtubeChannelID}&eventType=live&type=video&key=${currentYoutubeApiKey}`
+        const youtubeLink = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${this.channelName}&eventType=live&type=video&key=${currentYoutubeApiKey}`
         const response = await fetch(youtubeLink, {
             headers,
             cache: 'no-store'
@@ -33,68 +33,43 @@ export default class YoutubeAnnouncer extends BaseAnnouncer {
                 const [item] = data.items
                 if (item.id && item.id.videoId) {
                     if (!this.queue.includes(item.id.videoId)) {
-                        console.log(`[youtube] Video ${item.id.videoId} found, trying to send a message to the channel ${discordChannelId}`)
-                        this.queue.push(item.id.videoId)
-                        this.sendMessage(item)
+                        const {snippet, id: {videoId}} = item
+                        const {channelTitle, title} = snippet
+                        const preview = snippet.thumbnails.high.url
+                        const stream_id = item.id.videoId
+                        
+                        this.channelNameFormat = channelTitle
+                        this.queue.push(stream_id)
+                        this.sendMessage({
+                            title,
+                            preview,
+                            videoId
+                        }, stream_id)
                     } else {
-                        console.log('[youtube] An alert has already been created about this stream, skip')
+                        this.log('An alert has already been created about this stream, skip')
                     }
                 } else {
-                    console.log('[youtube] Video id not found, skip')
+                    this.log('Video id not found, skip')
                 }
             } else {
                 console.log(JSON.stringify(data))
-                console.log('[youtube] New streams not found, waiting for the next iteration')
+                this.log('New streams not found, waiting for the next iteration')
             }
         } catch (error) {
             console.error(error)
         }
     }
     
-    sendMessage(streamData) {
-        const discordChannel = this.client.channels.cache.get(discordChannelId)
-        if (discordChannel) {
-            const {snippet, id: {videoId}} = streamData
-            const {channelTitle, title} = snippet
-            
-            const link = 'https://youtu.be/' + videoId
-            const {name, avatar} = webhook_user
-            const embed = new MessageEmbed()
-                .setTitle(title)
-                .setAuthor({
-                    name: `${channelTitle}`,
-                    url: link
-                })
-                .setColor('#f00')
-                .setURL(link)
-                .setImage(snippet.thumbnails.high.url)
-                .setFooter({
-                    iconURL,
-                    text: 'Стрим на Youtube'
-                })
-    
-            discordChannel.createWebhook(name, {avatar})
-                .then(async context => {
-                    console.log('[youtube] Sending a message')
-                    await context.send({
-                        content: '@here',
-                        embeds: [embed]
-                    })
-                    await context.delete()
-                })
-        }
-    }
-    
     async runQueue(_client) {
         this.client = _client
-        if (!youtubeChannelID) {
+        if (!this.channelName) {
             return console.error('[youtube] You didn\'t fill in the channel name')
         }
         if (!youtubeApiKeys || youtubeApiKeys.length === 0) {
             return console.error('[youtube] You didn\'t fill in the api keys')
         }
         
-        const interval = youtube_timeout_limit / youtubeApiKeys.length
+        const interval = this.interval / youtubeApiKeys.length
         await this.checkStream()
         setInterval(this.checkStream, interval)
     }
